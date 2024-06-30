397cdee15efd39c03c9ba6b8046d01fa95d53204
Paul Charlton <techguru@byiq.com>
2024-06-30 14:42:59 +0700

    checkpoint

--- tools/asahi-remove-partitions.sh
+++ tools/asahi-remove-partitions.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
+# shellcheck shell=bash3.2
 #
-# inspired to undo that which wsa done by https://fedora-asahi-remix.org
-#         curl https://fedora-asahi-remix.org/install | sh
+# clean up an installation of Asahi Linux by:
+# 1. removing all partitions created by the Asahi Linux installer, and
+# 2. resizing the preceding APFS GPT partition to re-occupy the free space resulting from [1], and
+# 3. removing `iSCPPreboot` information related to the Asahi Linux installer, and
+# 4. blessing the currently running macOS as the default boot OS
 #
+# if it seems like a lot of code to accomplish something seeming so simple,
+# the primary factor driving the code size is that it is intensely paranoid
+# about preventing the accidental removal of something valuable to the machine's
+# users which was not created during the Asahi Linux install process.
+#
+# currently, cleanup will fail prior to commencing if:
+# 1) there is more than one ESP (EFI System Partition)
+# 2) there are more than 4 contiguous partitions not installed for macOS
+#
+# bibliographic information:
+# 1) https://ss64.com/mac/bless.html
+# 2) https://support.apple.com/guide/security/boot-process-secac71d5623/web
+# 3)
 
-# Exit script if you tru to use an uninitialized variable.
+# bash configuration:
+# 1) Exit script if you tru to use an uninitialized variable.
 set -o nounset
 
-# Exit script if a statement returns a non-true return value.
+# 2) Exit script if a statement returns a non-true return value.
 set -o errexit
 
-# Use the error status of the first failure, rather than that of the last item in a pipeline.
+# 3) Use the error status of the first failure, rather than that of the last item in a pipeline.
 set -o pipefail
 
-#
-# https://ss64.com/mac/bless.html
-#
-# systemsetup -getstartupdisk
-#
-# https://support.apple.com/guide/security/boot-process-secac71d5623/web
-#
+## arguments
+declare -rx OUTER_SCRIPT="${OUTER_SCRIPT:-${0}}"
+
+# @@
 # ideally, each partition would have a label indicated which OS installed it, and why.
 # alas, GPT does not support such user-defined labels, nor does APFS.
 # a manifest per install would be nice ... seems like m1n1 or uboot should be given that
@@ -28,10 +43,11 @@ set -o pipefail
 # Otherwise, it will be possible to create a unique APFS subvolume whose entire purpose
 # is to use its APFS volume name as a label which is a concatenation of all partitions
 # involved in the particular installation as a simple manifest
-# until then, use a heurestic of one APFS partition followed by three naked GPT partitions
+# until then, use a heuristic of one APFS partition followed by three naked GPT partitions
 # and remove all 4 of those
 #
 
+# @@
 function rough_draft() {
 	.
 # df / |grep '^/dev' | sed -e 's| .*||' -e 's|\/dev\/||'
@@ -43,12 +59,13 @@ function rough_draft() {
 
 function abort_with_error() {
 	local -r error_message="${1}"
-	printf 'asahi-remove-partitions failed with status: %s\n' "${error_message}" > /dev/stderr
+	local -r error_code="${2}"
+	printf '%s failed with status: %s errno=%d\n' "${OUTER_SCRIPT}" "${error_message}" "${error_code}" > /dev/stderr
 	exit 1
 }
 
 #
-# heuristics assumpts designed to play safe when we do not have an explicit manifest
+# heuristics assumptions designed to play safe when we do not have an explicit manifest
 # of disk objects to be deleted
 #
 
@@ -64,7 +81,7 @@ function expected_non_apfs_partition_count() {
 	printf '3'
 }
 
-function verify_prerequisites() {
+function verify_cleaner_prerequisites() {
 	command -v brew
 	command -v jq
 	command -v diskutil
@@ -125,12 +142,13 @@ function find_physical_internal_whole_disks() {
 }
 
 function verify_physical_internal_whole_disk() {
+  local physical_internal_whole_disks
 	# returns DeviceIdentifier of internal drive if one drive in system
 	IFS=$'\n' read -r -d '' -a physical_internal_whole_disks <<< "$(find_physical_internal_whole_disks)"
 	printf '%s\n' "${physical_internal_whole_disks[@]}" 1>&2
 	printf '%s' "${physical_internal_whole_disks[0]}"
 	local -r count_internal_whole_disks="${#physical_internal_whole_disks[@]}"
-	[[ ${count_internal_whole_disks} = $(expected_physical_disk_count) ]] || abort_with_error 'incorrect number of whole disks'
+	[[ ${count_internal_whole_disks} = $(expected_physical_disk_count) ]] || abort_with_error 'incorrect number of whole disks' $?
 }
 
 function find_non_apfs_gpt_partitions() {
@@ -143,7 +161,7 @@ function verify_non_apfs_gpt_partitions() {
 	IFS=$'\n' read -r -d '' -a non_apfs_partitions <<< "$(find_non_apfs_gpt_partitions "${physical_disk}" )"
 	printf '%s \n' "${non_apfs_partitions[@]}" 1>&2
 	local -r count_non_apfs_partitions="${#non_apfs_partitions[@]}"
-	[[ ${count_non_apfs_partitions} = $(expected_non_apfs_partition_count) ]] || abort_with_error 'incorrect number of non-APFS partitions'
+	[[ ${count_non_apfs_partitions} = $(expected_non_apfs_partition_count) ]] || abort_with_error 'incorrect number of non-APFS partitions' $?
 	printf 'Verified [%d] non-apfs partitions\n' "${count_non_apfs_partitions}"
 }
 
@@ -163,10 +181,10 @@ function verify_apfs_gpt_partition() {
 	IFS=$'\n' read -r -d '' -a apfs_partitions <<< "$(find_apfs_gpt_partitions "${physical_disk}" )"
 	printf '%s \n' "${apfs_partitions[@]}" 1>&2
 	local -r count_apfs_partitions="${#apfs_partitions[@]}"
-	[[ ${count_apfs_partitions} = $(expected_apfs_partition_count) ]] || abort_with_error 'incorrect number of APFS partitions'
+	[[ ${count_apfs_partitions} = $(expected_apfs_partition_count) ]] || abort_with_error 'incorrect number of APFS partitions' $?
 	printf 'Verified [%d] apfs partitions\n' "${count_apfs_partitions}"
 	asahi_apfs_partition="${apfs_partitions[2]}"
-	verify_asahi_apfs_partition "${asahi_apfs_partition}" || abort_with_error 'unable to verify contents of asahi partition'
+	verify_asahi_apfs_partition "${asahi_apfs_partition}" || abort_with_error 'unable to verify contents of asahi partition' $?
 	printf 'Verified [%s] asahi apfs partition\n' "${asahi_apfs_partition}"
 }
 
@@ -181,22 +199,23 @@ function remove_asahi_non_apfs_gpt_partitions() {
 	local -r physical_disk="${1}"
 	IFS=$'\n' read -r -d '' -a non_apfs_partitions <<< "$(find_non_apfs_gpt_partitions "${physical_disk}" )"
 	printf '%s \n' "${non_apfs_partitions[@]}" 1>&2
+	local partition
 	for partition in "${non_apfs_partitions[@]}"; do
-		echo "removing: $partition with \"diskutil eraseVolume free free $partition\""
-		diskutil eraseVolume free free $partition
+		echo "removing: ${partition} with 'diskutil eraseVolume free free ${}partition}'"
+		diskutil eraseVolume free free "${partition}"
 	done
 }
 
 function remove_asahi_apfs_partition() {
 	local -r target_partition="${1}"
-	echo "removing: asahi apfs partition [${asahi_apfs_partition}] with \"diskutil apfs deleteContainer ${asahi_apfs_partition}\""
-	diskutil apfs deleteContainer ${asahi_apfs_partition}
+	echo "removing: asahi apfs partition [${target_partition}] with \"diskutil apfs deleteContainer ${asahi_apfs_partition}\""
+	diskutil apfs deleteContainer "${target_partition}"
 }
 
 function resize_apfs_partition() {
 	local -r target_partition="${1}"
 	echo "resizing: partition [${target_partition}] with \"diskutil apfs resizeContainer ${target_partition} 0\""
-	diskutil apfs resizeContainer ${target_partition} 0
+	diskutil apfs resizeContainer "${target_partition}" 0
 }
 
 function remove_asahi_apfs_gpt_partitions_and_resize_predecessor() {
@@ -217,16 +236,38 @@ function remove_asahi_partitions() {
 function start_with_macos() {
 	printf 'You may see a password prompt.  Applying your administrative password will allow MacOS to be'
 	printf 'the default Startup Disk on the next reboot\n'
-	bless --mount / --setBoot || abort_with_error 'Failed to (re)set startup disk'
+	bless --mount / --setBoot || abort_with_error 'Failed to (re)set startup disk' $?
 }
 
-function asahi_remove_partitions() {
-	verify_prerequisites 1>&2 || abort_with_error 'Failed to verify prerequisites'
-	verify_asahi_partition_assumptions || abort_with_error 'Could not verify previous asahi installation'
-	remove_asahi_partitions || abort_with_error 'Could not remove the asahi partitions'
-	start_with_macos || abort_with_error 'Failed to set MacOS as the startup OS'
-	reboot || abort_with_error 'Failed to reboot'
+function warn_user() {
+  printf 'You may be prompted by sudo to increase the privileges from your\n'
+  printf 'user default to those needed in order to correctly clean out\n'
+  printf 'the desired Asahi Linux installation.\n'
+  printf 'if you do not intend to remove an Asahi Linux installation, please\n'
+  printf 'Press Control-C, otherwise, press <Enter>'
+
+  read
 }
 
-asahi_remove_partitions #2> /dev/null
+function asahi_linux_cleaner_privileged() {
+	verify_cleaner_prerequisites 1>&2 || abort_with_error 'Failed to verify cleaner prerequisites' $?
+	verify_asahi_partition_assumptions || abort_with_error 'Could not verify previous asahi installation' $?
+
+	printf 'Verified assumptions needed and commencing cleanup\n'
+	abort_with_error 'Test mode -- aborting before cleanup' 0
+
+	remove_asahi_partitions || abort_with_error 'Could not remove the asahi partitions' $?
+	start_with_macos || abort_with_error 'Failed to set MacOS as the startup OS' $?
+	reboot || abort_with_error 'Failed to reboot' $?
+}
+
+function asahi_linux_cleaner() {
+  warn_user
+  sudo bash -c "$(declare -f); OUTER_SCRIPT=${OUTER_SCRIPT} asahi_linux_cleaner_privileged"
+}
+
+#
+# entry point is here
+#
+asahi_linux_cleaner #2> /dev/null
 
